a52532e35db4f60484c9b83fed038bc2955b66fa
BDLY <137902109+BDLY777@users.noreply.github.com>
2026-08-26 17:20:52 +0200

    Delete backend directory

--- backend/saved/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import SavedPost
-
-
-class SavedPostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SavedPost
-        fields = "__all__"
